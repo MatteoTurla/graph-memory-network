c305368f7b2b515e7ae10732e605cc60957597a3
matteo turla <mturla13@gmail.com>
2021-02-16 16:04:52 +0100

    restored

---
+++ model/MemoryAggregator.py
@@ -0,0 +1,42 @@
+import torch
+import math
+
+class MemoryAggregator(torch.nn.Module):
+  def __init__(self, input_size, n_heads):
+
+    super().__init__()
+
+    self.input_size = input_size
+    self.n_head = n_heads
+    self.head_size = input_size // n_heads
+    self.dk = math.sqrt(self.head_size)
+    
+    # query matrix
+    Wq = torch.Tensor(self.input_size, self.head_size)
+    torch.nn.init.xavier_uniform_(Wq)
+    self.Wq = torch.nn.Parameter(Wq)
+    # key matrix
+    Wk= torch.Tensor(self.input_size, self.head_size)
+    torch.nn.init.xavier_uniform_(Wk)
+    self.Wk = torch.nn.Parameter(Wk)
+    # value matrix
+    Wv = torch.Tensor(self.input_size, self.head_size)
+    torch.nn.init.xavier_uniform_(Wv)
+    self.Wv = torch.nn.Parameter(Wv)
+
+  def forward(self, X, edge_index):
+    Q = torch.matmul(X, self.Wq)
+    K = torch.matmul(X, self.Wk)
+    V = torch.matmul(X, self.Wv)
+
+    r = edge_index[0,:]
+    c = edge_index[1, :]
+
+    sparse_A = torch.sum(Q[r] * K[c], axis=-1)
+    dense_A = torch.sparse.FloatTensor(edge_index, sparse_A)
+
+    masked_A = torch.div(dense_A, self.dk)
+
+    attention = torch.matmul(torch.sparse.softmax(masked_A, dim=1).to_dense(), V)
+
+    return attention
---
+++ model/MultiHeadAttentionLayer.py
@@ -0,0 +1,37 @@
+import torch
+from collections import OrderedDict
+from model.MemoryAggregator import MemoryAggregator
+
+class MultiHeadAttentionLayer(torch.nn.Module):
+  def __init__(self, input_size, n_heads):
+    super().__init__()
+
+    self.input_size = input_size
+    self.n_heads = n_heads
+    
+    self.heads = torch.nn.ModuleList([MemoryAggregator(self.input_size, self.n_heads) for i in range(self.n_heads)])
+
+    # multi-head matrix
+    W0 = torch.Tensor(self.input_size, self.input_size)
+    torch.nn.init.xavier_uniform_(W0)
+    self.W0 = torch.nn.Parameter(W0)
+
+    self.normalization_layer1 = torch.nn.LayerNorm(self.input_size)
+    self.normalization_layer2 = torch.nn.LayerNorm(self.input_size)
+
+    self.feed_forward = torch.nn.Sequential(OrderedDict([
+          ('fc1', torch.nn.Linear(self.input_size, 512)),
+          ('relu1', torch.nn.ReLU()),
+          ('fc2', torch.nn.Linear(512, self.input_size)),
+        ]))
+
+  def forward(self, X, edge_index):
+    results = [head(X, edge_index) for head in self.heads]
+
+    embedding  = torch.matmul(torch.cat(results, dim=1), self.W0)
+    add_normalize = self.normalization_layer1(X + embedding)
+
+    ff = self.feed_forward(add_normalize)
+    add_normalize2 = self.normalization_layer2(add_normalize + ff)
+
+    return add_normalize2
