0a0b7f65661104f48d0b21e9c94220b5c0df5091
matteo turla <mturla13@gmail.com>
2021-02-16 17:49:09 +0100

    cuda device

--- main.py
+++ main.py
@@ -8,19 +8,23 @@ if __name__ == "__main__":
 
     name = "PubMed"
     dataset = Planetoid(root='./tmp/'+name, name=name)
-    print("number of features: ", dataset.num_classes)
+    print("number of features: ", dataset.num_features)
+    print("number of classes: ", dataset.num_classes)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("device: ", device)
 
     dataloader = DataLoader(dataset, 0.4, 9, 3)
     
-    model = GraphMemoryNetwork(128, 4, 2, 3)
-    print(model.train())
+    model = GraphMemoryNetwork(128, 4, 2, 3).to(device)
+    #print(model.train())
 
     optimizer = torch.optim.Adam(model.parameters(), weight_decay=0.01)
     criterion = torch.nn.CrossEntropyLoss()
 
     for e in range(30):
-        loss, train_acc = train(model, dataloader.train_loader, criterion, optimizer, "cpu")
+        loss, train_acc = train(model, dataloader.train_loader, criterion, optimizer, device)
         if e % 3 == 0:
-            train_acc, test_acc = test(model, dataloader.subgraph_loader, dataloader.graph, "cpu")
+            train_acc, test_acc = test(model, dataloader.subgraph_loader, dataloader.graph, device)
             print(loss, train_acc, test_acc)
     
