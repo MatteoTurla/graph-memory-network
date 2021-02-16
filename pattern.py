0c7431663a33aa486302855e6dc1bf4b5bdcbc39
matteo turla <mturla13@gmail.com>
2021-02-16 19:25:47 +0100

    pattern

--- pattern.py
+++ pattern.py
@@ -4,6 +4,7 @@ from torch_geometric.data import DataLoader
 import torch
 
 # overwrite train function in utils, we do not have, mask there
+from torch_geometric.utils import add_self_loops
 def train(model, loader, criterion, optimizer, device):
     model.train()
 
@@ -32,9 +33,30 @@ def train(model, loader, criterion, optimizer, device):
 
     return total_loss/total_batch, total_correct/total_example
 
+@torch.no_grad()
+def test(model, loader, device):
+    model.eval()
+
+    total_example = total_correct = 0.0
+
+    for data in loader:
+        x = data.x.to(device)
+        edge_index = add_self_loops(data.edge_index)[0].to(device)
+        
+        # in case y is a n x 1 tensor (ogb graph)
+        y = data.y.squeeze().to(device)
+
+        out = model(x, edge_index)
+
+        total_example += out.shape[0]
+        total_correct += out.argmax(dim=-1).eq(y).sum().item()
+
+    return total_correct/total_example
+
 if __name__ == "__main__":
 
     dataset = GNNBenchmarkDataset(root='tmp/pattern', name="PATTERN")
+    dataset_val = GNNBenchmarkDataset(root='tmp/pattern_val', name="PATTERN", split="val")
 
     print("number of features: ", dataset.num_features)
     print("number of classes: ", dataset.num_classes)
@@ -43,8 +65,9 @@ if __name__ == "__main__":
     print("device: ", device)
 
     dataloader = DataLoader(dataset, batch_size=128)
+    dataloader_val = DataLoader(dataset_val, batch_size=128)
     
-    model = GraphMemoryNetwork(dataset.num_features, 4, 2, dataset.num_classes).to(device)
+    model = GraphMemoryNetwork(dataset.num_features, 5, 1, dataset.num_classes).to(device)
     #print(model.train())
 
     optimizer = torch.optim.Adam(model.parameters(), weight_decay=0.01)
@@ -52,5 +75,6 @@ if __name__ == "__main__":
 
     for e in range(30):
         loss, train_acc = train(model, dataloader, criterion, optimizer, device)
-        print(loss, train_acc)
+        val_acc = test(model, dataloader_val, device)
+        print(loss, train_acc, val_acc)
     
