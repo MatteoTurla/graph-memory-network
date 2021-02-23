c000307ae442f7f39e7a9df93b01377ae4bab2b3
matteo turla <mturla13@gmail.com>
2021-02-23 12:51:07 +0100

    stable sparse softmax

--- Planetoid.py
+++ Planetoid.py
@@ -1,13 +1,13 @@
-from model.GraphMemoryNet_2 import GraphMemoryNetwork_2
+from model.GMN import GMNnode
 from utils.DataLoader import DataLoader
-from utils.utils import cluster_train, test
+from utils.utils import cluster_train, subgraph_test
 from torch_geometric.datasets import Planetoid
 from torch_geometric.transforms import NormalizeFeatures
 import torch
 
 if __name__ == "__main__":
 
-    name = "PubMed"
+    name = "Cora"
     dataset = Planetoid(root='tmp/'+name, name=name,
                         transform=NormalizeFeatures())
     print("number of features: ", dataset.num_features)
@@ -17,20 +17,20 @@ if __name__ == "__main__":
     print("device: ", device)
 
     # if test_size = None -> use standard mask
-    dataloader = DataLoader(dataset, num_parts=128,
-                            batch_size=32, test_size=0.5)
+    dataloader = DataLoader(dataset, num_parts=1,
+                            batch_size=1, test_size=0.5)
 
-    model = GraphMemoryNetwork_2(
+    model = GMNnode(
         dataset.num_features, dataset.num_classes, n_heads=2, n_layers=3).to(device)
     # print(model.train())
 
-    optimizer = torch.optim.Adam(model.parameters(), weight_decay=0.01)
+    optimizer = torch.optim.Adam(model.parameters(), weight_decay=0.1)
     criterion = torch.nn.CrossEntropyLoss()
 
-    for e in range(30):
+    for e in range(300):
         loss, train_acc = cluster_train(
             model, dataloader.train_loader, criterion, optimizer, device)
-        if e % 3 == 0:
-            train_acc, test_acc = test(
+        if e % 10 == 0:
+            train_acc, test_acc = subgraph_test(
                 model, dataloader.subgraph_loader, dataloader.graph, device)
             print(loss, train_acc, test_acc)
--- model/MemoryAggregator.py
+++ model/MemoryAggregator.py
@@ -2,6 +2,15 @@ import torch
 import math
 
 
+def stable_neighbors_softmax(a, edge_index, n_nodes):
+    # a = Q_dot_Kt_stable
+    a1 = a.exp()
+    a2 = torch.zeros(n_nodes, dtype=torch.float).scatter_add_(
+        0, edge_index[0], a1)
+    a3 = a2[edge_index[0]]
+    return a1 / a3
+
+
 class MemoryAggregator(torch.nn.Module):
     def __init__(self, input_size, n_heads):
 
@@ -22,15 +31,20 @@ class MemoryAggregator(torch.nn.Module):
         K = self.Wk(X)
         V = self.Wv(X)
 
-        sparse_A = torch.sum(Q[edge_index[0, :]] *
+        Q_dot_Kt = torch.sum(Q[edge_index[0, :]] *
                              K[edge_index[1, :]], axis=-1)
+        # divide by the column dimension of K
+        Q_dot_Kt_stable = torch.div(Q_dot_Kt, self.dk)
 
-        dense_A = torch.sparse.FloatTensor(
-            edge_index, sparse_A, torch.Size([X.shape[0], X.shape[0]]))
+        # masked_A = torch.sparse.FloatTensor(
+        #    edge_index, Q_dot_Kt_stable, torch.Size([X.shape[0], X.shape[0]]))
 
-        masked_A = torch.div(dense_A, self.dk)
+        # softmax_A = torch.sparse.softmax(masked_A, dim=1)
+        softmax_A = stable_neighbors_softmax(
+            Q_dot_Kt_stable, edge_index, X.shape[0])
+        sparse_softmax_A = torch.sparse.FloatTensor(
+            edge_index, softmax_A, torch.Size([X.shape[0], X.shape[0]]))
 
-        attention = torch.sparse.mm(
-            torch.sparse.softmax(masked_A, dim=1), V)
+        attention = torch.sparse.mm(sparse_softmax_A, V)
 
         return attention
